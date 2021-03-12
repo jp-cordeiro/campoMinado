ffbce8faa35c7a845a165f1d18a3e139faae1a03
JP <jpc.araujo@hotmail.com>
2021-03-11 23:25:17 -0300

    add logic of game for App

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
-import Field from './src/components/Field';
+import {SafeAreaView, Text, StyleSheet, Alert} from 'react-native';
 import MineField from './src/components/MineField/MineField';
-import {createMineBoard} from './src/functions';
+import {
+  cloneBoard,
+  createMinedBoard,
+  hadExplosion,
+  openField,
+  showMines,
+  wonGame,
+} from './src/functions';
 
 import './src/params';
 import params from './src/params';
 
 const App = () => {
   const [board, setBoard] = useState([]);
+  const [won, setWon] = useState(false);
+  const [lose, setLose] = useState(false);
 
   useEffect(() => {
-    const cols = params.getColumnsAmount();
-    const rows = params.getRowsAmount();
-    const createBoard = createMineBoard(rows, cols, minesAmount());
-    setBoard(createBoard);
+    if (board.length === 0) {
+      const cols = params.getColumnsAmount();
+      const rows = params.getRowsAmount();
+      const createBoard = createMinedBoard(rows, cols, minesAmount());
+      setBoard(createBoard);
+    }
   }, []);
 
   const minesAmount = () => {
@@ -23,13 +33,33 @@ const App = () => {
     return Math.ceil(cols * rows * params.difficultLevel);
   };
 
+  const onOpenField = (row, column) => {
+    const clonedBoard = cloneBoard(board);
+    openField(clonedBoard, row, column);
+    const lose = hadExplosion(clonedBoard);
+    const won = wonGame(clonedBoard);
+
+    if (lose) {
+      showMines(clonedBoard);
+      Alert.alert('Você perdeu!');
+    }
+
+    if (won) {
+      Alert.alert('Você venceu!!');
+    }
+
+    setBoard(clonedBoard);
+    setLose(lose);
+    setWon(won);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.welcome}>Iniciando o Campo Minado!</Text>
       <Text>
         Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}
       </Text>
-      <MineField board={board} />
+      <MineField board={board} onOpenField={onOpenField} />
     </SafeAreaView>
   );
 };
